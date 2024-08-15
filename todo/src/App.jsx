4eae707ec3c2866aed6860c07100a4c149310728
lipezxs <fa8561141@gmail.com>
2024-08-15 09:37:31 -0300

    enviando projeto para deploy

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -28,7 +28,6 @@ function App() {
   ])
 
   const [search, setSearch] = useState("");
-
   const [filter, setFilter] = useState("All");
   const [sort, setSort] = useState("Asc")
 
@@ -36,13 +35,13 @@ function App() {
     const newTodos = [
       ...todos,
       {
-      id: Math.floor(Math.random() * 10000),
-      text,
-      category,
-      isCompleted: false,
-    },
-  ];
-  setTodos(newTodos);
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+    setTodos(newTodos);
   };
 
   const removeTodo = (id) => {
@@ -53,7 +52,7 @@ function App() {
   const completeTodo = (id) => {
     const newTodos = [...todos]
     newTodos.map((todo) =>
-    todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
     );
     setTodos(newTodos);
 
@@ -61,20 +60,20 @@ function App() {
   return (
     <div className="app">
       <h1>Lista de tarefas</h1>
-      <Seacrh search={search} setSearch={setSearch}/>
-      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
+      <TodoForm addTodo={addTodo} />
+      <Seacrh search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
-        .filter((todo)=> filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
-        .filter((todo) => 
-          todo.text.toLowerCase().includes(search.toLowerCase())
-      )
-      .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
-      .map((todo) => (
-        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
-        ))}
+          .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
+          .map((todo) => (
+            <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
+          ))}
       </div>
-      <TodoForm addTodo={addTodo}/>
     </div>
   );
 }
--- todo/src/components/Seacrh.jsx
+++ todo/src/components/Seacrh.jsx
@@ -1,14 +1,14 @@
 
 
-    const Seacrh = ({search, setSearch}) => {
+const Seacrh = ({ search, setSearch }) => {
   return (
-  <div className="search">
-    <h2>Pesquisar</h2>
-    <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Digite para pesquisar..." />
+    <div className="search">
+      <h2>Pesquisar</h2>
+      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Digite para pesquisar..." />
 
-  </div>
+    </div>
 
-   
+
   )
 }
 
