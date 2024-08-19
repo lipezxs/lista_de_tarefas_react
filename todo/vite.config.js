15e2a9a1c1ef2835fd24d962c611f30b599c539d
lipezxs <fa8561141@gmail.com>
2024-08-19 14:45:32 -0300

    enviando para deploy

--- todo/vite.config.js
+++ todo/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lista_de_tarefas_deploy",
+  base: "/lista_de_tarefas",
 })
